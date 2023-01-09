6bb5eb8f03b1e10fce45061cc56cd5e7c4dfb323
hayhyang <hayhyang@gmail.com>
2023-01-09 11:09:59 +0900

    [vue2/Introduction] Handling User Input

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
     <div>{{ message }}</div>
     <div v-bind:title="message">Hover your mouse over me for a few seconds
     to see my dynamically bound title!</div>
+    <button v-on:click="reverseMessage">Reverse Message</button>
+    <input v-model="message" />
     <div v-if="seen">Now you see me</div>
     <ul>
       <li v-for="(todo, i) in todos" :key="i">{{todo.text}}</li>
@@ -19,13 +21,18 @@ export default {
   },
   data() {
     return {
-      message: 'Hello World' + new Date().toLocaleString(),
+      message: 'Hello World',
       seen: true,
       todos: [
         {text: 'Learn Javascript'},
         {text: 'Learn Vue'},
         {text: 'Learn React'}
       ]
+    }
+  },
+  methods: {
+    reverseMessage:function () {
+      this.message = this.message.split('').reverse().join('')
     }
   }
 }
